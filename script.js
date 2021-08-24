7ddbfe47e7180a35403afada393939b85244f7cb
argenismahath <oleidergonzalez11mx@gmail.com>
2021-08-23 21:55:35 -0500

    version 3

---
+++ script.js
@@ -0,0 +1,19 @@
+let link1=document.getElementById("about");
+let link2=document.getElementById("project");
+let icoMenu=document.getElementById("icoMenu");
+let h1=document.getElementById('h1');
+
+window.addEventListener('resize', function(event){
+    let newWidth = window.innerWidth;
+    let newHeight = window.innerHeight; 
+    console.log(newWidth);
+
+    if (newWidth<751) {
+        
+    }
+    else{
+    
+    }
+});
+
+
