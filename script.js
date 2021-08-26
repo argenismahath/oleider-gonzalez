f686297b45b47b9b5067c785732748ad1db05728
argenismahath <oleidergonzalez11mx@gmail.com>
2021-08-26 01:39:45 -0500

    version 2

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ window.onscroll = function () {
     gsap.to(".icon-cog", {duration: "1.5", rotate: y });
 };
 
+// Animation
 
 ScrollReveal().reveal('#aboutMe', {delay: 500 });
 ScrollReveal().reveal('#about2', {delay: 700})
@@ -45,3 +46,42 @@ ScrollReveal().reveal('.imageSkills1', { delay: 500 });
 ScrollReveal().reveal('.imageSkills2', { delay: 700 });
 
 ScrollReveal().reveal('#Cv', { delay: 500 });
+
+
+document.getElementById("DownloadCv").addEventListener("click", lenguaje);
+
+
+function lenguaje() {
+    let lan=document.documentElement.lang;
+    let a=document.getElementById('DownloadCv');
+    if (lan=='es') {
+        a.href="cv/cv_oleider_2021-es.pdf";
+    }else{
+        a.href="cv/cv_oleider_2021 - english.pdf";
+    }
+}
+// arrowmenu
+
+document.getElementById("circlenavmenu").addEventListener("click", navMenu);
+
+var backgnav=document.getElementById('circlenavmenu');
+
+function navMenu() {
+
+    let menu= document.getElementById('DisplayMenu');
+
+
+    let togle=menu.classList.toggle('navArrowMenuDisplay');
+
+    
+    if (togle==true) {
+        menu.style.display='block';
+        backgnav.style.backgroundColor='rgba(20, 30, 87, 0.669)';
+
+        
+    }else{
+        menu.style.display='none';
+        backgnav.style.backgroundColor='rgba(43, 58, 140, 0.669)';
+    }
+
+}
