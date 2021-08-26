1b2f0d325009ea4bac2ff3ccb60b61df2ec363bb
argenismahath <oleidergonzalez11mx@gmail.com>
2021-08-26 17:03:48 -0500

    v Phone-modified

--- script.js
+++ script.js
@@ -2,15 +2,18 @@ window.addEventListener('resize', function (event) {
     let newWidth = window.innerWidth;
     let newHeight = window.innerHeight;
     console.log(newWidth);
-
     if (newWidth < 751) {
-
     }
     else {
-
+        
     }
+    
+
 });
 
+// menudesplegablecell
+let MenuPhoneDisplay =document.getElementById('icoMenu');
+MenuPhoneDisplay.addEventListener('click', navMenu);
 
 // animacionea
 
@@ -18,13 +21,13 @@ var tl = gsap.timeline();
 window.onscroll = function () {
     var y = window.scrollY;
     console.log(y);
-    gsap.to(".icon-cog", {duration: "1.5", rotate: y });
+    gsap.to(".icon-cog", { duration: "1.5", rotate: y });
 };
 
 // Animation
 
-ScrollReveal().reveal('#aboutMe', {delay: 500 });
-ScrollReveal().reveal('#about2', {delay: 700})
+ScrollReveal().reveal('#aboutMe', { delay: 500 });
+ScrollReveal().reveal('#about2', { delay: 700 })
 ScrollReveal().reveal('#projects', { delay: 500 });
 
 ScrollReveal().reveal('#cabanaBenyo', { delay: 500 });
@@ -52,36 +55,36 @@ document.getElementById("DownloadCv").addEventListener("click", lenguaje);
 
 
 function lenguaje() {
-    let lan=document.documentElement.lang;
-    let a=document.getElementById('DownloadCv');
-    if (lan=='es') {
-        a.href="cv/cv_oleider_2021-es.pdf";
-    }else{
-        a.href="cv/cv_oleider_2021 - english.pdf";
+    let lan = document.documentElement.lang;
+    let a = document.getElementById('DownloadCv');
+    if (lan == 'es') {
+        a.href = "cv/cv_oleider_2021-es.pdf";
+    } else {
+        a.href = "cv/cv_oleider_2021 - english.pdf";
     }
 }
 // arrowmenu
 
 document.getElementById("circlenavmenu").addEventListener("click", navMenu);
 
-var backgnav=document.getElementById('circlenavmenu');
+var backgnav = document.getElementById('circlenavmenu');
 
 function navMenu() {
 
-    let menu= document.getElementById('DisplayMenu');
+    let menu = document.getElementById('DisplayMenu');
 
 
-    let togle=menu.classList.toggle('navArrowMenuDisplay');
+    let togle = menu.classList.toggle('navArrowMenuDisplay');
 
-    
-    if (togle==true) {
-        menu.style.display='block';
-        backgnav.style.backgroundColor='rgba(20, 30, 87, 0.669)';
 
-        
-    }else{
-        menu.style.display='none';
-        backgnav.style.backgroundColor='rgba(43, 58, 140, 0.669)';
+    if (togle == true) {
+        menu.style.display = 'block';
+        backgnav.style.backgroundColor = 'rgba(20, 30, 87, 0.669)';
+
+
+    } else {
+        menu.style.display = 'none';
+        backgnav.style.backgroundColor = 'rgba(43, 58, 140, 0.669)';
     }
 
 }
