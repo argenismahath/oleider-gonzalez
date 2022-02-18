d43d0c8c98dc75e0b1ada649744c2d791a927f08
oleider gonzalez <oleidergonzalez11@gmail.com>
2022-02-18 01:55:14 -0600

    movil css

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
-
-let newWidth = window.innerWidth;
+let newWidth = screen.width;
 let newHeight = window.innerHeight;
-
-let backgroundsize=document.body.style.backgroundSize = newWidth + 'px, ' + newHeight + 'px';;
-
-window.addEventListener('resize', function (event) {
-    let newWidth = window.innerWidth;
-    let newHeight = window.innerHeight;
-    this.document.body.style.backgroundSize = newWidth + 'px ', newHeight + 'px';
-    console.log(newWidth);
-});
-
 
 // menudesplegablecell
 let MenuPhoneDisplay = document.getElementById('icoMenu');
@@ -37,15 +26,16 @@ ScrollReveal().reveal('#Cv', { delay: 500 });
 document.getElementById("contacts").addEventListener("click", lenguaje);
 
 function lenguaje() {
-    console.log('hola');
+
     let lan = document.documentElement.lang;
-    let a = document.getElementById('DownloadCv');
+    prompt(lan)
+
     if (lan == 'es') {
        let pdf= window.location.href="cv/cv_oleider_2022.pdf";
         download(pdf, "cv_oleider_2022.pdf");
-    } else {
-        let pdf= window.location.href="cv/cv_oleider_2021 - english.pdf";
-        download(pdf, "cv_oleider_2022.pdf");
+    } else if (lan == 'en') {
+        let pdf_en= window.location.href="cv/cv_oleider_2021_EN.pdf";
+        download(pdf_en, "cv_oleider_20222.pdf");
     }
 }
 // arrowmenu
@@ -53,29 +43,48 @@ function lenguaje() {
 document.getElementById("circlenavmenu").addEventListener("click", navMenu);
 
 var backgnav = document.getElementById('circlenavmenu');
+let menu = document.getElementById('DisplayMenu');
 
+counter=false;
 function navMenu() {
-    let menu = document.getElementById('DisplayMenu');
 
-    //Check if browser is Firefox 
-    if (navigator.userAgent.search("Firefox") >= 0) {
-        // insert conditional Firefox Code here
-        menu.style.backgroundColor='rgba(26, 49, 77, 0.8)';
-    }
+    if (newWidth >= 750) {
+        
+            //Check if browser is Firefox 
+            if (navigator.userAgent.search("Firefox") >= 0) {
+                // insert conditional Firefox Code here
+                menu.style.backgroundColor='rgba(26, 49, 77, 0.8)';
+            }
+        
+        
+        
+            menu.classList.toggle('navArrowMenuDisplay');
+        
+        
+            if (togle == true) {
+                menu.style.display = 'block';
+                // backgnav.style.backgroundColor = 'rgba(20, 30, 87, 0.669)';
+        
+        
+            } else {
+                menu.style.display = 'none';
+                // backgnav.style.backgroundColor = 'rgba(43, 58, 140, 0.669)';
+            }
+        }else{
 
 
+            menu.classList.add('navArrowMenuDisplay');
 
-    let togle = menu.classList.toggle('navArrowMenuDisplay');
+            if (counter == false) {
+                menu.style.transform='translateX(-100%)';
+                counter=true;
+               
+            }
+            else{
+                menu.style.transform='translateX(100%)';
+                counter=false;
 
-
-    if (togle == true) {
-        menu.style.display = 'block';
-        // backgnav.style.backgroundColor = 'rgba(20, 30, 87, 0.669)';
-
-
-    } else {
-        menu.style.display = 'none';
-        // backgnav.style.backgroundColor = 'rgba(43, 58, 140, 0.669)';
-    }
+            }
+        }
 
 }
